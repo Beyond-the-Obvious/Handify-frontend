79af62fbc9bf0f521f17f46dc296e7d3c448c6a3
bayanfuad <bayanjawawdeh9@gmail.com>
2022-08-15 15:13:18 +0300

    fix the footter

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -15,14 +15,13 @@ export default function App() {
           <span>Get connected with us on social networks:</span>
         </div>
 
-        {/* <div>
-          <a href="" className="me-4 text-reset"><MDBIcon fab icon="facebook-f" /></a>
-          <a href="" className="me-4 text-reset"><MDBIcon fab icon="twitter" /></a>
-          <a href="" className="me-4 text-reset"><MDBIcon fab icon="google" /></a>
-          <a href="" className="me-4 text-reset"><MDBIcon fab icon="instagram" /></a>
-          <a href="" className="me-4 text-reset"><MDBIcon fab icon="linkedin" /></a>
-          <a href="" className="me-4 text-reset"><MDBIcon fab icon="github" /></a>
-        </div> */}
+        <div>
+          <a href="https://web.facebook.com/" className="me-4 text-reset"><MDBIcon fab icon="facebook-f" /></a>
+          <a href="https://twitter.com/" className="me-4 text-reset"><MDBIcon fab icon="twitter" /></a>
+           <a href="https://www.instagram.com/" className="me-4 text-reset"><MDBIcon fab icon="instagram" /></a>
+          <a href="https://www.linkedin.com/" className="me-4 text-reset"><MDBIcon fab icon="linkedin" /></a>
+        
+        </div>
 
       </section>
 
@@ -31,31 +30,31 @@ export default function App() {
           <MDBRow className="mt-3">
             <MDBCol md="3" lg="4" xl="3" className="mx-auto mb-4">
               {/* <img src={logo} alt="Logo" /> */}
-              <p>Here you can use rows and columns to organize your footer content. Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              <p>Our website is a perfect place to support small buisness owners and provide the users with unique hand-made peices .</p>
             </MDBCol>
 
             <MDBCol md="2" lg="2" xl="2" className="mx-auto mb-4">
-              <h6 className="text-uppercase fw-bold mb-4">Products</h6>
-              <p><a href="#!" className="text-reset">Angular</a></p>
-              <p><a href="#!" className="text-reset">React</a></p>
-              <p><a href="#!" className="text-reset">Vue</a></p>
-              <p><a href="#!" className="text-reset">Laravel</a></p>
+              <h6 className="text-uppercase fw-bold mb-4">Provided products you can search or add</h6>
+              <p>Food items</p>
+              <p>Accessories</p>
+              <p>Art works</p>
+            
             </MDBCol>
 
             <MDBCol md="3" lg="2" xl="2" className="mx-auto mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
-              <p><a href="#!" className="text-reset">Pricing</a></p>
-              <p><a href="#!" className="text-reset">Settings</a></p>
-              <p><a href="#!" className="text-reset">Orders</a></p>
-              <p><a href="#!" className="text-reset">Help</a></p>
+              <p><a href="/About" className="text-reset">About us</a></p>
+              <p><a href="/Signup" className="text-reset">Sign up</a></p>
+              <p><a href="/Product" className="text-reset">Products</a></p>
+              {/* <p><a href="/Home" className="text-reset">Home</a></p> */}
             </MDBCol>
 
             <MDBCol md="4" lg="3" xl="3" className="mx-auto mb-md-0 mb-4">
               <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
-              <p><MDBIcon icon="home" className="me-2" />New York, NY 10012, US</p>
-              <p><MDBIcon icon="envelope" className="me-3" />info@example.com</p>
-              <p><MDBIcon icon="phone" className="me-3" /> + 01 234 567 88</p>
-              <p><MDBIcon icon="print" className="me-3" /> + 01 234 567 89</p>
+              <p><MDBIcon icon="home" className="me-2" />Amman,Jordan</p>
+              <p><MDBIcon icon="envelope" className="me-3" />info@handify.com</p>
+              <p><MDBIcon icon="phone" className="me-3" /> + 96279 345 567 8</p>
+              <p><MDBIcon icon="print" className="me-3" /> + 96278 234 674 8</p>
             </MDBCol>
 
           </MDBRow>
@@ -63,9 +62,10 @@ export default function App() {
       </section>
 
       <div className="text-center p-4" style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}>
-        © 2021 Copyright:
-        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+        © 2022 Copyright:
+        <a className="text-reset fw-bold" href="https://mdbootstrap.com/">Hadify.com</a>
       </div>
     </MDBFooter>
   );
 }
+
